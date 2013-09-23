d11b180b9e7067814b7cb06a728c93ab171c243e
Ralph Schaer <ralphschaer@gmail.com>
2013-09-23 10:05:14 +0200

    Rename starter class to better distinguish it in eclipse

--- src/main/java/Spring4WSStartTomcat.java
+++ src/main/java/Spring4WSStartTomcat.java
@@ -1,6 +1,6 @@
 import ch.rasc.embeddedtc.EmbeddedTomcat;
 
-public class StartTomcat {
+public class Spring4WSStartTomcat {
 	public static void main(String[] args) throws Exception {
 		EmbeddedTomcat.create().startAndWait();
 	}
