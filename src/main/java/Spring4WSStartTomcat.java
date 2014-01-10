6d4bfeeede37892d7662a5b13c94ae8b6635ef79
Ralph Schaer <ralphschaer@gmail.com>
2014-01-10 10:04:55 +0100

    Increase WebSocket text buffer size

--- src/main/java/Spring4WSStartTomcat.java
+++ src/main/java/Spring4WSStartTomcat.java
@@ -2,6 +2,6 @@ import ch.rasc.embeddedtc.EmbeddedTomcat;
 
 public class Spring4WSStartTomcat {
 	public static void main(String[] args) throws Exception {
-		EmbeddedTomcat.create().startAndWait();
+		EmbeddedTomcat.create().addContextInitializationParameter("org.apache.tomcat.websocket.textBufferSize", "1000000").startAndWait();
 	}
 }
