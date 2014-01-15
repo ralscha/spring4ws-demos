7f6361bc9ad27cf0417041125750fccb369096c2
Ralph Schaer <ralphschaer@gmail.com>
2014-01-15 13:32:19 +0100

    Increase websocket default buffer size. Some demos send and receive a lot of data

--- src/main/java/Spring4WSStartTomcat.java
+++ src/main/java/Spring4WSStartTomcat.java
@@ -2,6 +2,6 @@ import ch.rasc.embeddedtc.EmbeddedTomcat;
 
 public class Spring4WSStartTomcat {
 	public static void main(String[] args) throws Exception {
-		EmbeddedTomcat.create().addContextInitializationParameter("org.apache.tomcat.websocket.textBufferSize", "1000000").startAndWait();
+		EmbeddedTomcat.create().startAndWait();
 	}
 }
