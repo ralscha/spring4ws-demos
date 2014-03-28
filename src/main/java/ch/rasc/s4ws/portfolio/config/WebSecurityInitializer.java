3aa9d2c7f0cfda0807a0f477ced8f25f2383c0ac
Ralph Schaer <ralphschaer@gmail.com>
2014-03-28 08:36:38 +0100

    Spring 4.0.3 now has sensible defaults for channels

--- src/main/java/ch/rasc/s4ws/portfolio/config/WebSecurityInitializer.java
+++ src/main/java/ch/rasc/s4ws/portfolio/config/WebSecurityInitializer.java
@@ -18,8 +18,13 @@ package ch.rasc.s4ws.portfolio.config;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 /**
+ * ServletContext initializer for Spring Security specific configuration such as
+ * the chain of Spring Security filters.
+ * <p>
+ * The Spring Security configuration is customized with
+ * {@link org.springframework.samples.portfolio.config.WebSecurityConfig}.
+ *
  * @author Rob Winch
- * 
  */
 public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 	// nothing here
