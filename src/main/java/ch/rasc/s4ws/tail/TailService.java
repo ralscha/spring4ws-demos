95c53f66cc71fef79623eb91f6b0135c0dbe30ef
Ralph Schaer <ralphschaer@gmail.com>
2017-08-20 06:32:23 +0200

    Add support for ipv6 addresses

--- src/main/java/ch/rasc/s4ws/tail/TailService.java
+++ src/main/java/ch/rasc/s4ws/tail/TailService.java
@@ -154,7 +154,7 @@ public class TailService {
 	}
 
 	private static String getAccessLogRegex() {
-		String regex1 = "^([\\d.-]+)"; // Client IP
+		String regex1 = "^([\\d.:a-z]+)"; // Client IP
 		String regex2 = " (\\S+)"; // -
 		String regex3 = " (\\S+)"; // -
 		String regex4 = " \\[([\\w:/]+\\s[+\\-]\\d{4})\\]"; // Date
@@ -162,7 +162,7 @@ public class TailService {
 		String regex6 = " (\\d{3})"; // HTTP code
 		String regex7 = " (\\d+|.+?)"; // Number of bytes
 		String regex8 = " \"([^\"]+|.+?)\""; // Referer
-		String regex9 = " \"([^\"]+|.+?)\""; // Agent
+		String regex9 = " \"([^\"]*|.*?)\""; // Agent
 
 		return regex1 + regex2 + regex3 + regex4 + regex5 + regex6 + regex7 + regex8
 				+ regex9;
